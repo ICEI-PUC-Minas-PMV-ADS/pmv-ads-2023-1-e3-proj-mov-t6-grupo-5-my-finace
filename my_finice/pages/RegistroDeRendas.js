29515d18023d0a0cf0f1f621bab1e9c8e7472007
mthszf99 <matheus.pires@sga.pucminas.br>
2023-03-27 18:39:51 -0300

    criacao rendas

---
+++ my_finice/pages/RegistroDeRendas.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RegistroDeRendas = () => {
+
+    return (
+
+        <View></View>
+
+    );
+
+
+}
+
+export default RegistroDeRendas;
