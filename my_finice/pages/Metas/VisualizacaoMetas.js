9e173bd0f806c45187ae9885c7b78f81925c6d87
Matheus Sampaio <matheuslemos98@gmail.com>
2023-06-04 02:05:28 -0300

    Adição Botão para incrementar a porcentagem da meta

--- my_finice/pages/Metas/VisualizacaoMetas.js
+++ my_finice/pages/Metas/VisualizacaoMetas.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View,Text,FlatList,StatusBar, TouchableOpacity} from 'react-native';
+import { StyleSheet, View,Text,FlatList,StatusBar, TouchableOpacity, Alert} from 'react-native';
 import{List} from 'react-native-paper';
 import { ProgressBar} from 'react-native-paper';
 import{useEffect, useState} from 'react';
@@ -9,50 +9,84 @@ import { CreateTable,getMetas } from '../../Services/MetasDB';
 import React from 'react';
 
 function Metas() {
-    const [tituloMeta, setTituloMeta] = useState([])
-    const IsFocused = useIsFocused();
-    const navigation = useNavigation();
-    useEffect (()=>{
-        CreateTable()
-    },[])
+  const [valorAdicional, setValorAdicional] = useState({});
+  const [tituloMeta, setTituloMeta] = useState([]);
+  const IsFocused = useIsFocused();
+  const navigation = useNavigation();
 
-    async function verTabela(){
-        useEffect(() => {
-            getMetas().then((dados) => {
-                setTituloMeta(dados)
-            });
-        },[IsFocused])
+  useEffect(() => {
+    CreateTable();
+  }, []);
+
+  useEffect(() => {
+    verTabela();
+  }, [IsFocused]);
+
+  async function verTabela() {
+    const dados = await getMetas();
+    setTituloMeta(dados);
+  }
+
+  const adicionarValor = (item) => {
+    const valorAtual = valorAdicional[item.id] || 0;
+    const novoValor = valorAtual + item.poupar_mes;
+    setValorAdicional({
+      ...valorAdicional,
+      [item.id]: novoValor,
+    });
+
+    if (novoValor >= item.valor_meta) {
+      Alert.alert('Meta Concluída!');
     }
-    const Item = ({item}) => (
-        <TouchableOpacity style={aviso.item} onPress={() => navigation.navigate('EditarMetas',{id:item.id})}>
-        <List.Icon style={aviso.iconePosicao}/>
-        <View style={aviso.lista}>
-            <Text>{item.titulo_meta}</Text>
-            <Text>{'Valor: R$' + item.valor_meta}</Text>
-            <Text>{'Descrição Meta: ' + item.descricao_meta}</Text>
-            <Text>{'Valor a ser Poupado: R$ ' + item.poupar_mes}</Text>
-            <Text>{(item.valor_meta/item.poupar_mes)+ '% da sua meta completa'}</Text>
-        </View>
-        <List.Icon style={aviso.verMais} icon={'chevron-right'} color={'green'}/>
-        </TouchableOpacity>
-    );
+  };
 
-    verTabela()
+  const Item = ({ item }) => {
+    const valorAtual = valorAdicional[item.id] || 0;
+    const porcentagemMeta = (valorAtual / item.valor_meta) * 100 || 0;
+
     return (
-        <View>
-            <CbSemVolta title="Metas"/>
-            <View>
-                <FlatList
-                    data={tituloMeta}
-                    renderItem={Item}
-                    keyExtractor={item => item.id}
-                />
-            </View>
-            <Botaoflutuante
-                onPress={() => navigation.navigate('RegistroMetas')}
-            />
+      <TouchableOpacity
+        style={aviso.item}
+        onPress={() => navigation.navigate('EditarMetas', { id: item.id })}
+      >
+        <List.Icon style={aviso.iconePosicao} />
+        <View style={aviso.lista}>
+          <Text>{item.titulo_meta}</Text>
+          <Text>{'Valor: R$' + item.valor_meta}</Text>
+          <Text>{'Descrição Meta: ' + item.descricao_meta}</Text>
+          <Text>{'Valor a ser Poupado: R$ ' + item.poupar_mes}</Text>
+          <Text>{`${porcentagemMeta}% da sua meta completa`}</Text>
+          <TouchableOpacity
+            onPress={() => adicionarValor(item)}
+            style={{
+              backgroundColor: 'green',
+              padding: 5,
+              marginTop: 10,
+              width: 102,
+              borderRadius: 8,
+            }}
+          >
+            <Text style={{ color: 'white' }}>Adicionar Valor</Text>
+          </TouchableOpacity>
         </View>
+        <List.Icon style={aviso.verMais} icon={'chevron-right'} color={'green'} />
+      </TouchableOpacity>
     );
+  };
+
+  return (
+    <View>
+      <CbSemVolta title="Metas" />
+      <View>
+        <FlatList
+          data={tituloMeta}
+          renderItem={Item}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+      <Botaoflutuante onPress={() => navigation.navigate('RegistroMetas')} />
+    </View>
+  );
 }
 const aviso = StyleSheet.create({
     av:{
